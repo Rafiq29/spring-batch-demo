b3f76f4631f200704f8291245b12fec612011776
Rafiq29 <rafiq.babayev.29@gmail.com>
2022-12-05 13:57:53 +0400

    -- Batch service created

---
+++ src/main/resources/schema-all.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS coffee;
+
+CREATE TABLE coffee
+(
+    coffee_id       SERIAL PRIMARY KEY,
+    brand           VARCHAR(20),
+    origin          VARCHAR(20),
+    characteristics VARCHAR(30)
+);
